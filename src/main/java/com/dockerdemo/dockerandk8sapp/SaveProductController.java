84cf2463388126a0c7de84fcb4a5171e77131c32
Megha Sisode <megha.sisode@modmed.com>
2023-06-16 05:12:05 -0400

    added endpoint to save the product data to a file

--- src/main/java/com/dockerdemo/dockerandk8sapp/SaveProductController.java
+++ src/main/java/com/dockerdemo/dockerandk8sapp/SaveProductController.java
@@ -1,2 +1,31 @@
-package com.dockerdemo.dockerandk8sapp;public class SaveProductController {
+package com.dockerdemo.dockerandk8sapp;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
+public class SaveProductController {
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@RequestBody String productDetails) throws IOException {
+        Path fileName = Path.of(
+            "/Users/megha.sisode/workspace/DockerAndK8sApp/productInfo.txt");
+        try (BufferedWriter out = new BufferedWriter(
+                new FileWriter(String.valueOf(fileName), true))) {
+                out.write(productDetails);
+                out.close();
+
+            return "Data saved to file successfully..!!!";
+        }
+        catch (IOException e) {
+            return "Exception occurred while saving data to file." + e;
+        }
+    }
 }
